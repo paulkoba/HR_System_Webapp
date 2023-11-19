f059062e08f0e607db8addf8400c72d944c4e4f9
Paul Koba <paul.koba@pm.me>
2023-11-19 00:03:53 +0000

    base setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
 import {useEffect, useState} from "react";
-const API_URL = process.env.REACT_APP_API;
+const API_URL = "http://127.0.0.1:8080/api";
 
 function App() {
   const [data, setData] = useState("No data :(");
   
   useEffect(() => {
     async function getData() {
-      const url = `${API_URL}/hello`;
-      const response = await fetch(url);
-      const data = await response.json();
-      setData(data.msg);
+        const url = `${API_URL}/hello`;
+        const response = await fetch(url);
+
+        //const data = await response.json();
+        setData(await response.text());
+
     }
     getData();
   }, []); 
 
   return (
-    <>
+    <main>
       <h1>MERN App!</h1>
       <p>Data from server: {data}</p>
-    </>
+    </main>
   );
 }
 
